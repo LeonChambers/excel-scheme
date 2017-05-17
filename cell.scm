a3efa7e5628fcc79fdd123b5b2291b8e6a96084d
Leon Chambers <leon8556@gmail.com>
2017-05-17 02:41:26 -0400

    Switched to more general backend

--- cell.scm
+++ cell.scm
@@ -1,88 +1,219 @@
 #|
-This file defines the basic cell structure of the excel system.
-Cells are the atomic unit of the propagator network. These cells know
-their coordinates, which could be defined to allow for rows or
-columns, inputs and value. The cells also have knowledge of their
-"func" which is only used to display the function that is used to
-extract value, while eliding the aspects that ensure inputs are
-calculated properly.
-|#
 
+This file defines the basic cell structure of the excel system.
+
+Cells are the atomic unit of the propagator network. These cells know
+
+their coordinates, which could be defined to allow for rows or
+
+columns, inputs and value. The cells also have knowledge of their
+
+"func" which is only used to display the function that is used to
+
+extract value, while eliding the aspects that ensure inputs are
+
+calculated properly.
+
+
+|# 
 (define cells (make-equal-hash-table)) 
 
+
+(define current-n 7)
 (define-record-type <cell>
-  (%make-cell coords)
-  cell?
-  (coords cell-coords)
-  (inputs cell-inputs %set-cell-inputs!)
-  (func cell-func %set-cell-func!)
-  (val %cell-val %set-cell-val!)
-  (version %cell-version %set-cell-version!))
+    (%make-cell coords inputs value func is-func)
+    cell?
+  (coords %get-coords)
+  (inputs get-inputs set-inputs!)
+  (value %get-value %set-value!)
+  (func get-func %set-func!)
+  (is-func is-func? %set-is-func))
 
-;; Returns the cell with coordinated coords. If there is no cell with
-;; those coordinates, it creates one and returns it
-(define (get-cell coords)
-  (let ((cell (hash-table/get cells coords #f)))
-    (if (not cell)
-	(let ((new-cell (%make-cell coords)))
-	  (hash-table/put! cells coords cell)
-	  new-cell)
-	cell)))
+; Default value of the default cell
+(define default-value (lambda args 0))
+(define default-func 0)
 
-(define (cell-val cell)
-  (update-cell! cell)
-  (%cell-val (get-cell (cell-coords cell))))
 
-(define last-version-number 1)
+; Default Value for a cell when no cell is found
+(define default-cell
+  (begin (hash-table/put! cells (list 0 0)
+			  (%make-cell (list 0 0) '() default-value default-func #f))
+	 (hash-table/get cells (list 0 0) 0)))
 
-(define (version-number)
-  (let ((prev-val last-version-number))
-    (set! last-version-number (+ prev-val 1))
-    prev-val))
+; Returns the cell at x,y or default cell if none is found
+; Note: This will not find a column cell that overlaps with a cell
+; coordinates. Meaning if (1,1) is passed but only (1,y) exists then
+; the default value will be returned.
+(define (get-cell x y)
+  (hash-table/get cells (list x y) default-cell))
 
-(let ((curr-num 0))
-  (define (version-number)
-    (set! curr-num (+ curr-num 1))
-    curr-num))
+(define (get-cell-coords coords)
+  (get-cell (car coords) (cadr coords)))
 
-(define (set-cell-val! cell val)
-  (%set-cell-val! cell val)
-  (%set-cell-func! cell #f)
-  (%set-cell-inputs! cell #f)
-  (%set-cell-version! cell (version-number))
-  (hash-table/put! cells (cell-coords cell) cell))
+; Adds a cell to the "sheet" with the given values then returns that cell
+(define (add-cell coords inputs func)
+  (hash-table/put! cells coords (%make-cell coords inputs func func #f))
+  (get-cell (car coords) (cadr coords)))
 
-(define (set-cell-func! cell func inputs)
-  (%set-cell-val! cell #f)
-  (%set-cell-func! cell func)
-  (%set-cell-inputs! cell inputs)
-  (%set-cell-version! cell 0)
-  (hash-table/put! cells (cell-coords cell) cell))
+; Gets the coordinates of a cell, taking not of context
+(define (get-coords cell . context)
+  (let ((coords (%get-coords cell)))
+    (if (not (null? context))
+	(convert-coords coords (car context))
+	(convert-coords coords))))
 
-;; If this cell doesn't have a function, does nothing. Otherwise,
-;; recurses on all input cells, and then if necessary, recomputes the
-;; value of this cell based on the new values of the input cells
-(define (update-cell! cell)
-  (let ((func (cell-func cell)))
-    ;; If this is a static value, we don't have to do anything
-    (if func
-	(let* ((version (%cell-version cell))
-	       (inputs (cell-inputs cell))
-	       (input-cells (map get-cell (map cdr inputs)))
-	       (max-input-version
-		(reduce-left max 1 (map %cell-version input-cells))))
-	  ;; Recurse on all input cells
-	  (map update-cell! input-cells)
-	  ;; If one of the inputs have been changed since the last
-	  ;; time this cell was updated, we have to recompute the
-	  ;; value of this cell
-	  (if (> max-input-version version)
-	      ;; Recompute the value of this cell
-	      (let* ((environment (extend-top-level-environment
-				   (nearest-repl/environment)
-				   (map car inputs)
-				   (map %cell-val input-cells)))
-		     (new-val (eval func environment)))
-		(%set-cell-val! cell new-val)
-		(%set-cell-version! cell max-input-version)
-		(hash-table/put! cells (cell-coords cell) cell)))))))
+(define coord-handlers '())
+
+(define (add-coord-handler tag converter deconverter)
+  (set! coord-handlers (cons (list tag converter deconverter) coord-handlers)))
+
+(add-coord-handler 'cell
+		   (lambda (coords) coords)
+		   (lambda (coords context) coords))
+(add-coord-handler 'row
+		   (lambda (coords) (list 'x (cadr coords)))
+		   (lambda (coords context)
+		     (if (equal? (car coords) 'x)
+			 (list (car context) (cadr coords))
+			 coords)))
+(add-coord-handler 'col
+		   (lambda (coords) (list (car coords) 'y))
+		   (lambda (coords context)
+		     	(if (equal? (cadr coords) 'y)
+			    (list (car coords) (cadr context))
+			    coords)))
+
+(define (handler-tag handlers)
+  (caar handlers))
+
+(define (handler-converter handlers)
+  (cadar handlers))
+
+(define (handler-deconverter handlers)
+  (caddar handlers))
+
+(define (get-dominant-cell coords)
+  (define (helper handlers)
+    (if (null? handlers)
+	(get-cell-coords coords)
+	(let* ((handler (handler-converter handlers))
+	       (new-coords (handler coords))
+	       (cell (get-cell-coords new-coords)))
+	  (if (or (equal? cell default-cell)
+		  (equal? (%get-value cell) default-value))
+	      (helper (cdr handlers))
+	      (begin
+		cell)))))
+  (helper coord-handlers))
+
+(define (convert-coords coords . context)
+  (if (null? context)
+      coords
+      (let ((context (car context)))
+	(define (helper coords handlers)
+	  (if (null? handlers)
+	      coords
+	      (let ((new-coords
+		     ((handler-deconverter handlers) coords context)))
+		(helper new-coords (cdr handlers)))))
+	(let ((final-coords (helper coords coord-handlers)))
+	  (if (equal? final-coords context)
+	      'called-self
+	      final-coords)))))
+
+; Evaluates and returns a cells value
+(define (get-value cell context)
+  (let ((initial-val (%get-value cell)))
+    (initial-val context)))
+
+; Gets the value at a given coordinates
+(define (get-value-coords coords)
+  (let ((cell (get-dominant-cell coords)))
+    (get-value cell coords)))
+
+; This gets the cell at this point, or if the cell doesn't exits then
+; it creates a cell at the given location with the default value
+(define (get-or-set-cell x y)
+  (let (( retrieved (hash-table/get cells (list x y) 0)))
+    (if (equal? retrieved 0)
+	(add-cell (list x y) '() default-value)
+	retrieved)))
+
+; Returns a list of (func . inputs) of a given cell
+(define (get-innards coords)
+  (let* ((cell (get-dominant-cell coords)))
+    (list (get-func cell)
+	  (get-inputs celll))))
+
+; Given coordinates a function and inputs sets the given cell to have
+; the given values
+(define (set-innards! coords func . inputs)
+  (set-cell! (car coords) (cadr coords) func inputs))
+
+
+(define (%add-input! cell input-val label)
+  (let ((old-inputs (get-inputs cell)))
+    (if (assq label old-inputs)
+	(del-assq! label old-inputs))
+    (let ((new-inputs (cons(append (list label) input-val) old-inputs)))
+      (hash-table/modify! cells (get-coords cell) 'default
+			  (lambda (cell)
+			    (set-inputs! cell new-inputs)
+			    cell)))))
+
+; Adds an input to a cell with the given label
+(define (add-input! x y x1 y1 label)
+  (%add-input! (get-cell x y) (list x1 y1) label))
+
+; Iterates through inputs and adds them to the cell
+(define (add-inputs! cell inputs)
+  (if (null? inputs)
+      '()
+      (let ((label (caar inputs))
+	    (input (cdar inputs))
+	    (rest (cdr inputs)))
+	(%add-input! cell input label)
+	(cons label (add-inputs! cell rest)))))
+; Sets the cell at x y to have the given value
+(define (set-cell! x y value . args)
+  (if (procedure? value)
+      (set-cell-func! x y value args)
+      (set-cell-val! x y value)))
+
+
+; Sets the value of a cell directly to the value given
+(define (set-cell-val! x y value)
+  (let ((cell (get-or-set-cell x y)))
+    (%set-is-func cell #f)
+    (%set-value! cell (lambda args value))
+    (%set-func! cell value)))
+
+; Sets a cells value with a shorter name
+(define (sv! x y value)
+  (set-cell! x y value))
+
+(define (set-cell-func! x y func args)
+  (let* ((cell (get-or-set-cell x y))
+	 (arg-labels (add-inputs! cell args))
+	 (inputs (get-inputs cell))
+	 (arg-inputs (get-subset arg-labels inputs)))
+    (let ((new-func (lambda (context)
+		      (let* ((input-coords (map cdr arg-inputs))
+			     (input-values (map get-value-coords
+						input-coords)))
+			 (let* ((environment (extend-top-level-environment
+					      (nearest-repl/environment)
+					      arg-labels
+					      input-values)))
+			   (eval func environment))))))
+      (%set-is-func cell #t)
+      (%set-value! cell new-func)
+      (%set-func! cell func))))
+
+(define plus-str (read (open-input-string "(lambda args (apply + args))")))
+
+;(make-n-by-n 7)
+;(set-lambda! 'x 1 plus-str '((a x 2)))
+;(add-input! 1 1 1 2 'A)
+;(add-input! 1 1 1 3 'B)
+;(add-input! 1 1 1 4 'C)
--- main.scm
+++ main.scm
@@ -3,31 +3,31 @@
 (define (main)
   (init-layout!
    (lambda (x y)
-     (let ((val (cell-val (get-cell (list x y)))))
+     (let ((val (get-value-coords (list x y))))
        (if val (number->string val) "")))
    (lambda (x y)
-     (let* ((cell (get-cell (list x y)))
-	    (func (cell-func cell))
-	    (inputs (cell-inputs cell))
-	    (val (cell-val cell)))
-       (if func
+     (let* ((cell (get-dominant-cell (list x y)))
+	    (func (get-func cell))
+	    (inputs (get-inputs cell))
+	    (val (get-value-coords (list x y))))
+       (if (is-func? cell)
 	   (string-append "="
 			  (with-output-to-string
 			    (lambda ()
 			      (write (cons func inputs)))))
 	   (if val (number->string val) ""))))
    (lambda (x y val)
-     (let ((cell (get-cell (list x y))))
-       (if (eq? (string-ref val 0) #\=)
-	   (let* ((parsed-val
-		   (read (open-input-string (string-tail val 1))))
-		  (func (car parsed-val))
-		  (inputs (cdr parsed-val)))
-	     (set-cell-func! cell func inputs))
-	   (set-cell-val! cell (eval (read (open-input-string val))
-				     (nearest-repl/environment))))))
+     (if (eq? (string-ref val 0) #\=)
+	 (let* ((parsed-val
+		 (read (open-input-string (string-tail val 1))))
+		(func (car parsed-val))
+		(inputs (cdr parsed-val)))
+	   (set-cell-func! x y func inputs))
+	 (begin
+	   (set-cell-val! x y (eval (read (open-input-string val))
+				      (nearest-repl/environment))))))
    (lambda (cmd) (pp (eval cmd (nearest-repl/environment)))))
-  (init-graphics!)
-  (run-graphics))
-
+  (init-graphics!)  (run-graphics))
 (main)
+
+
